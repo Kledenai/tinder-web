b64570d41da77e677c0b04072418d6cb6ac1e933
Kledenai <bruno.silva1643@etec.sp.gov.br>
2019-08-09 07:02:58 -0300

    tinder-web finalizado

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.svg';
 import like from '../../assets/like.svg';
 import dislike from '../../assets/dislike.svg';
@@ -19,32 +20,53 @@ export default function Main({ match }){
         }
 
         loadUsers();
-    }, 
-    [match.params.id])
+    }, [match.params.id]);
+
+    async function handleLike(id) {
+        await api.post(`/devs/${id}/likes`, null, {
+            headers: { user: match.params.id }
+        });
+
+        setUsers(users.filter(user => user._id != id));
+    }
+
+    async function handleDislike(id) {
+        await api.post(`/devs/${id}/dislikes`, null, {
+            headers: { user: match.params.id },
+        });
+
+        setUsers(users.filter(user => user._id != id));
+    }
 
     return(
         <div className="container-main">
-            <img src={logo} alt="tindev"/>
-            <ul>
-                {users.map(user => (
-                    <li key={user._id}>
-                        <img src={user.avatar} alt={user.name}/>
-                        <footer>
-                            <strong>{user.name}</strong>
-                            <p>{user.bio}</p>
-                        </footer>
-
-                        <div className="buttons">
-                            <button type="button">
-                                <img src={dislike} alt="dislike" />
-                            </button>
-                            <button type="button">
-                                <img src={like} alt="like" />
-                            </button>
-                        </div>
-                    </li>
-                ))}
-            </ul>
+            <Link to="/">
+                <img src={logo} alt="tindev"/>
+            </Link>
+            { users.length > 0 ? (
+                <ul>
+                { users.map(user => (
+                        <li key={user._id}>
+                            <img src={user.avatar} alt={user.name}/>
+                            <footer>
+                                <strong>{user.name}</strong>
+                                <p>{user.bio}</p>
+                            </footer>
+    
+                            <div className="buttons">
+                                <button type="button" onClick={() => handleDislike(user._id)}>
+                                    <img src={dislike} alt="dislike" />
+                                </button>
+                                <button type="button" onClick={() => handleLike(user._id)}>
+                                    <img src={like} alt="like" />
+                                </button>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <div class="empt">Acabou :(</div>
+            )}
         </div>
     );
 }
