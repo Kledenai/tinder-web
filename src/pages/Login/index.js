a94ccb1a2f6d893cf13b8e54f9a14f6b0d8e63ed
Kledenai <bruno.silva1643@etec.sp.gov.br>
2019-08-09 05:35:03 -0300

    configurando login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../assets/logo.svg';
 import './login.css';
 
 export default function Login(){
+    const [username, setUsername] = useState('');
+
+    function handleSubmit(e){
+        e.preventDefault();
+
+        console.warn(username);
+    }
+
     return(
         <div className="container-login">
             <img src={logo} alt="tindev"/>
-            <form className="form-login">
-                <input className="input" placeholder="Digite seu usuário no Github"/>
+            <form className="form-login" onSubmit={handleSubmit}>
+                <input 
+                    className="input" 
+                    placeholder="Digite seu usuário no Github"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                />
                 <button className="button">logar</button>
             </form>
         </div>
