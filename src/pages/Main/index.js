72b8d13f4340a439b8fc62acac41e785798c3775
Kledenai <bruno.silva1643@etec.sp.gov.br>
2019-08-09 06:30:50 -0300

    já está listando os usuários

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import logo from '../../assets/logo.svg';
+import like from '../../assets/like.svg';
+import dislike from '../../assets/dislike.svg';
+import api from '../../services/api';
 
-export default function Main(){
+import './main.css';
+
+export default function Main({ match }){
+    const [ users, setUsers ] = useState([]);
+
+    useEffect(() => {
+        async function loadUsers(){
+            const response = await api.get('/devs', {
+                headers: { user: match.params.id, }
+            })
+
+            setUsers(response.data);
+        }
+
+        loadUsers();
+    }, 
+    [match.params.id])
+
     return(
-        <h1>Hello World</h1>
+        <div className="container-main">
+            <img src={logo} alt="tindev"/>
+            <ul>
+                {users.map(user => (
+                    <li key={user._id}>
+                        <img src={user.avatar} alt={user.name}/>
+                        <footer>
+                            <strong>{user.name}</strong>
+                            <p>{user.bio}</p>
+                        </footer>
+
+                        <div className="buttons">
+                            <button type="button">
+                                <img src={dislike} alt="dislike" />
+                            </button>
+                            <button type="button">
+                                <img src={like} alt="like" />
+                            </button>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
 }
